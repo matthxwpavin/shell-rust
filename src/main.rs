4d0a949ae37507d1803c047c979a9f65d0f0c6d7
Pavin Wannasirilak <Matthxw@MacBook-Pro.local>
2025-05-12 17:45:49 +0700

    REPL supported

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process;
 
 fn main() {
     // Uncomment this block to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
 
-    // Wait for user input
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    println!("{}: command not found", input.trim());
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        // Wait for user input
+        let mut input = String::new();
+        if let Err(err) = io::stdin().read_line(&mut input) {
+            eprintln!("Could not read a command: {:?}", err);
+            process::exit(1);
+        }
+        println!("{}: command not found", input.trim());
+    }
 }
